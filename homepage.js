840dc7348f2ae8e112a0dca783c450e00b155fd1
felixqian <felix.qian@ximalaya.com>
2021-07-01 19:01:25 +0800

    add js

--- homepage.js
+++ homepage.js
@@ -1,20 +1,20 @@
 function login() {
-    // window.location.href = 'login.html'
     var userName = document.getElementById("userName").value;
     var userPassword = document.getElementById("userPassword").value;
+    console.log("jump");
     // alert("提交成功" + "\n 用户名：" + userName + "\n密码: " + userPassword);
     window.localStorage.setItem("userName", userName);
     window.localStorage.setItem("userPassword", userPassword);
-    // window.location.href = 'login.html'
-    setTimeout("javascript:location.href='login.html'", 5000); 
 }
 
 function getUserName() {
     var userName  = window.localStorage.getItem("userName");
-    document.getElementById("userName").innerHTML = userName;
+    // document.write(userName);
+    document.getElementById("p").innerHTML = '<p>用户名: ' + userName.toString() + '</p>'
 }
 
 function getUserName() {
     var userPassword  = window.localStorage.getItem("userPassword");
-    document.getElementById("userPassword").innerHTML = userPassword
+    // document.write(userPassword);
+    document.getElementById("p").innerHTML = '<p>密码: ' + userPassword.toString() + '</p>'
 }
